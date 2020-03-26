c2504be697db8f8afc58c7107b9e9c07262943a2
Josh <61505761+JoshingNotJoking@users.noreply.github.com>
2020-03-26 18:20:53 -0400

    Fix some issues with auto updating

--- main.lua
+++ main.lua
@@ -1,15 +1,14 @@
-getNewestVersion = loadstring("httpGet(https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/version.lua"))
+-- Check to make sure script is up to date. Move to a function file later.
+getNewestVersion = loadstring(httpGet("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/version.lua"))
 latestVersion = getNewestVersion()
-currentVersion = dofile(localPath .."/lib/version.lua")
-print (currentVersion)
-print (latestVersion)
+currentVersion = dofile(scriptPath() .."/lib/version.lua")
 
 function automaticUpdates ()
   if currentVersion == latestVersion then
     toast ("You are up to date!")
   else
-    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/main.lua", localPath .."main.lua")
-    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/version.lua", localPath .."/lib/version.lua")
+    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/main.lua", scriptPath() .."main.lua")
+    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/version.lua", scriptPath() .."/lib/version.lua")
     scriptExit("You have Updated SinBot!")
   end
 end
