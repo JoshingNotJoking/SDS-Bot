978720b45c55035cfbc04435e007b18be5c3d0d0
Josh <61505761+JoshingNotJoking@users.noreply.github.com>
2020-03-26 17:15:52 -0400

    Update repo name and files referencing that name

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-getNewestVersion = loadstring(httpGet(https://raw.githubusercontent.com/JoshingNotJoking/SDS-Bot/master/lib/version.lua"))
+getNewestVersion = loadstring(httpGet(https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/version.lua"))
 latestVersion = getNewestVersion()
 currentVersion = dofile(localPath .."/lib/version.lua")
 print (currentVersion)
@@ -8,8 +8,8 @@ function automaticUpdates ()
   if currentVersion == latestVersion then
     toast ("You are up to date!")
   else
-    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SDS-Bot/master/main.lua", localPath .."main.lua")
-        httpDownload("https://raw.githubusercontent.com/Paladiex/Palbot/master/lib/version.lua", localPath .."/lib/version.lua")
+    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/main.lua", localPath .."main.lua")
+    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/version.lua", localPath .."/lib/version.lua")
     scriptExit("You have Updated SinBot!")
   end
 end
