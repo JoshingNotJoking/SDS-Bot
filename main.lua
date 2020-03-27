594b78cccc1a6a0ec15fbe941f54b5cd743ef506
Josh <61505761+JoshingNotJoking@users.noreply.github.com>
2020-03-26 22:59:58 -0400

    Test out image clicking and syncing

--- main.lua
+++ main.lua
@@ -9,9 +9,19 @@ function automaticUpdates ()
   else
     httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/main.lua", scriptPath() .."main.lua")
     httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/version.lua", scriptPath() .."/lib/version.lua")
+    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/regions.lua", scriptPath() .."/lib/regions.lua")
+    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/imageupdater.lua", scriptPath() .."imageupdater.lua")
     scriptExit("You have Updated SinBot!")
   end
 end
 
 
 automaticUpdates ();
+
+-- Just an example to learn how clicking works. I don't think this is efficient because it's searching the entire screen.
+while true do
+  if existsClick(Pattern("areaTavern.png")) then
+    toast ("It worked!")
+    wait(8);
+  end
+end
