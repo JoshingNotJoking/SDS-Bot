65db8a0d5f89254fbabcf5cbb8adbf33900d1f31
Josh <61505761+JoshingNotJoking@users.noreply.github.com>
2020-03-26 23:01:12 -0400

    Version bump
    Hoping to trigger a download of assets to test image syncing

---
+++ lib/imageupdater.lua
@@ -0,0 +1,2 @@
+imagePath = (scriptPath() .. "/image/1280x720")
+httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/areaTavern.png", imagePath .."areaTavern.png")
