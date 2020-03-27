adc8a7b843aa9d5aeac856d9d62604e7123138d1
Josh <61505761+JoshingNotJoking@users.noreply.github.com>
2020-03-26 23:32:26 -0400

    Setup localPath global

--- main.lua
+++ main.lua
@@ -1,17 +1,18 @@
 -- Check to make sure script is up to date. Move to a function file later.
+localPath = scriptPath()
 getNewestVersion = loadstring(httpGet("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/version.lua"))
 latestVersion = getNewestVersion()
-currentVersion = dofile(scriptPath() .."/lib/version.lua")
+currentVersion = dofile(localPath .."/lib/version.lua")
 setImagePath(localPath .. "1280x720")
 
 function automaticUpdates ()
   if currentVersion == latestVersion then
     toast ("You are up to date!")
   else
-    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/main.lua", scriptPath() .."main.lua")
-    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/version.lua", scriptPath() .."/lib/version.lua")
-    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/regions.lua", scriptPath() .."/lib/regions.lua")
-    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/imageupdater.lua", scriptPath() .."imageupdater.lua")
+    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/main.lua", localPath .."main.lua")
+    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/version.lua", localPath .."/lib/version.lua")
+    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/regions.lua", localPath .."/lib/regions.lua")
+    httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/imageupdater.lua", localPath .."imageupdater.lua")
     scriptExit("You have Updated SinBot!")
   end
 end
