7bac60a1bb4267afec35e5f985eaeec86835ea2e
Josh <61505761+JoshingNotJoking@users.noreply.github.com>
2020-03-29 13:05:37 -0400

    Allow setup to create missing directories

--- setup.lua
+++ setup.lua
@@ -1,17 +1,19 @@
+luaLib = loadstring(httpGet("https://raw.githubusercontent.com/mercobots/luaLib/master/luaLib.lua"))
 localPath = scriptPath()
 imgPath = localPath .. "image/1280x720"
 setImagePath(imgPath)
+luaLib()
 
 dialogInit()
 -- GUI
-addTextView("  ") addTextView("Network Functions in Ankulua settings must be enabled for this to work.") newRow()
-addTextView("  ") addTextView("Create the following directories before running this script:") newRow()
-addTextView("  ") addTextView("- image") newRow()
-addTextView("  ") addTextView("- image/1280x720") newRow()
-addTextView("  ") addTextView("- lib") newRow()
-addTextView("  ") addTextView("- lib/dialogs") newRow()
+addTextView("  ") addTextView("Network Functions in Ankulua settings must be enabled for this to work.")
 
 dialogShow("Important")
+
+mkdir(localPath .. "image");
+mkdir(localPath .. "image/1280x720");
+mkdir(localPath .. "lib");
+mkdir(localPath .. "lib/dialogs");
 
 function assetSetup ()
   -- Download Scripts
