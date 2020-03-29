9a591a3dea22bdeb4fd1652f2787cc897e693ed9
Josh <61505761+JoshingNotJoking@users.noreply.github.com>
2020-03-29 19:52:04 -0400

    Select correct team for Guild Boss

--- lib/guildBoss.lua
+++ lib/guildBoss.lua
@@ -1,3 +1,18 @@
 function guildBossHard ()
-  scriptExit("Guild Boss Farming coming soon!");
+  local nextArrow = Location(780, 150);
+
+  if guildBossTeamSelection == 1 then guildBossTeamSelection = "teamOne"
+  elseif guildBossTeamSelection == 2 then guildBossTeamSelection = "teamTwo"
+  elseif guildBossTeamSelection == 3 then guildBossTeamSelection = "teamThree"
+  elseif guildBossTeamSelection == 4 then guildBossTeamSelection = "teamFour"
+  elseif guildBossTeamSelection == 5 then guildBossTeamSelection = "teamFive"
+  end
+
+  if exists(Pattern(guildBossTeamSelection .. ".png"):similar(0.93)) then
+    toast("Correct Team chosen");
+  else
+    repeat
+      click(nextArrow);
+    until exists(Pattern(guildBossTeamSelection .. ".png"):similar(0.93))
+  end
 end
