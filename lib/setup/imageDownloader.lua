be2fdcdef229bec074b133345f8d94a69c8d5088
Josh <61505761+JoshingNotJoking@users.noreply.github.com>
2020-03-29 14:13:02 -0400

    Refactor for clarity

--- lib/setup/imageDownloader.lua
+++ lib/setup/imageDownloader.lua
@@ -1,51 +1,50 @@
 -- ========================
--- Image Updater
+-- Image Downloader
 -- ========================
 -- Credit to Palbot for the implementation: https://github.com/Paladiex/Palbot
---
 
 localPath = scriptPath()
-imagePath = (localPath .. "image/1280x720/")
+imagesPath = (localPath .. "images/1280x720/")
 
-function downloadImages()
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/areaQuestReward.png", imagePath .."areaQuestReward.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/areaRepeatResults.png", imagePath .."areaRepeatResults.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/areaStageSelect.png", imagePath .."areaStageSelect.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/autoRepeatOff.png", imagePath .."autoRepeatOff.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/battleButton.png", imagePath .."battleButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/battleEquipButton.png", imagePath .."battleEquipButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/closeBattleButton.png", imagePath .."closeBattleButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/closeDeathMatch.png", imagePath .."closeDeathMatch.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/closeRepeatResults.png", imagePath .."closeRepeatResults.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/completeGearButton.png", imagePath .."completeGearButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/continueQuestButton.png", imagePath .."continueQuestButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/deathMatchPrompt.png", imagePath .."deathMatchPrompt.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearAttack.png", imagePath .."gearAttack.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearCrit.png", imagePath .."gearCrit.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearCritRes.png", imagePath .."gearCritRes.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearDefense.png", imagePath .."gearDefense.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearGoButton.png", imagePath .."gearGoButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearHealth.png", imagePath .."gearHealth.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearRecovery.png", imagePath .."gearRecovery.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearStartButton.png", imagePath .."gearStartButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/tavernGlobalButton.png", imagePath .."tavernGlobalButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/stageHardButton.png", imagePath .."stageHardButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/stageStartButton.png", imagePath .."stageStartButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/areaInventoryPrompt.png", imagePath .."areaInventoryPrompt.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/closeEquipmentFull.png", imagePath .."closeEquipmentFull.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearApplyButton.png", imagePath .."gearApplyButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearCommonIcon.png", imagePath .."gearCommonIcon.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearUncommonIcon.png", imagePath .."gearUncommonIcon.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearRareIcon.png", imagePath .."gearRareIcon.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/gearSuperRareIcon.png", imagePath .."gearSuperRareIcon.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/menuBackArrow.png", imagePath .."menuBackArrow.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/registerAllButton.png", imagePath .."registerAllButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/salvageButton.png", imagePath .."salvageButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/stageStartButton.png", imagePath .."stageStartButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/salvageConfirmButton.png", imagePath .."salvageConfirmButton.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/salvageIcon.png", imagePath .."salvageIcon.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/salvageMissingEquip.png", imagePath .."salvageMissingEquip.png");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/image/1280x720/salvageSuccessButton.png", imagePath .."salvageSuccessButton.png");
+function downloadimages()
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/areaQuestReward.png", imagesPath .."areaQuestReward.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/areaRepeatResults.png", imagesPath .."areaRepeatResults.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/areaStageSelect.png", imagesPath .."areaStageSelect.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/autoRepeatOff.png", imagesPath .."autoRepeatOff.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/battleButton.png", imagesPath .."battleButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/battleEquipButton.png", imagesPath .."battleEquipButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/closeBattleButton.png", imagesPath .."closeBattleButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/closeDeathMatch.png", imagesPath .."closeDeathMatch.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/closeRepeatResults.png", imagesPath .."closeRepeatResults.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/completeGearButton.png", imagesPath .."completeGearButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/continueQuestButton.png", imagesPath .."continueQuestButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/deathMatchPrompt.png", imagesPath .."deathMatchPrompt.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearAttack.png", imagesPath .."gearAttack.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearCrit.png", imagesPath .."gearCrit.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearCritRes.png", imagesPath .."gearCritRes.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearDefense.png", imagesPath .."gearDefense.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearGoButton.png", imagesPath .."gearGoButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearHealth.png", imagesPath .."gearHealth.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearRecovery.png", imagesPath .."gearRecovery.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearStartButton.png", imagesPath .."gearStartButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/tavernGlobalButton.png", imagesPath .."tavernGlobalButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/stageHardButton.png", imagesPath .."stageHardButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/stageStartButton.png", imagesPath .."stageStartButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/areaInventoryPrompt.png", imagesPath .."areaInventoryPrompt.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/closeEquipmentFull.png", imagesPath .."closeEquipmentFull.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearApplyButton.png", imagesPath .."gearApplyButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearCommonIcon.png", imagesPath .."gearCommonIcon.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearUncommonIcon.png", imagesPath .."gearUncommonIcon.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearRareIcon.png", imagesPath .."gearRareIcon.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/gearSuperRareIcon.png", imagesPath .."gearSuperRareIcon.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/menuBackArrow.png", imagesPath .."menuBackArrow.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/registerAllButton.png", imagesPath .."registerAllButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/salvageButton.png", imagesPath .."salvageButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/stageStartButton.png", imagesPath .."stageStartButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/salvageConfirmButton.png", imagesPath .."salvageConfirmButton.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/salvageIcon.png", imagesPath .."salvageIcon.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/salvageMissingEquip.png", imagesPath .."salvageMissingEquip.png");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/images/1280x720/salvageSuccessButton.png", imagesPath .."salvageSuccessButton.png");
 end
 
-downloadImages ()
+downloadimages ()
--- lib/setup/scriptDownloader.lua
+++ lib/setup/scriptDownloader.lua
@@ -1,12 +1,22 @@
+-- ========================
+-- Script Downloader
+-- ========================
+-- Credit to Palbot for the implementation: https://github.com/Paladiex/Palbot
+
 localPath = scriptPath();
 
 function downloadScripts()
+  -- /
   httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/main.lua", localPath .."main.lua");
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/setup.lua", localPath .."setup.lua");
+  -- /lib
+  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/gearFarm.lua", localPath .."/lib/gearFarm.lua");
   httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/version.lua", localPath .."/lib/version.lua");
   httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/regions.lua", localPath .."/lib/regions.lua");
-  httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/gearFarm.lua", localPath .."/lib/gearFarm.lua");
+  -- /lib/dialogs
   httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/dialogs/gearFarm.lua", localPath .."/lib/dialogs/gearFarm.lua");
   httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/dialogs/gui.lua", localPath .."/lib/dialogs/gui.lua");
+  -- /lib/setup
   httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/setup/imageDownload.lua", localPath .."/lib/setup/imageDownload.lua");
   httpDownload("https://raw.githubusercontent.com/JoshingNotJoking/SinBot/master/lib/setup/scriptDownload.lua", localPath .."/lib/setup/scriptDownload.lua");
 end
